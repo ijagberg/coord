91608e326551e083b7ad718f3d0261962afc8d45
Isak Jägberg <ijagberg@gmail.com>
2020-05-16 11:08:35 +0200

    Fix failing tests

--- src/lib.rs
+++ src/lib.rs
@@ -17,13 +17,20 @@ where
     T: Num + ToPrimitive + Copy,
 {
     /// Calculate the Euclidean distance (straight line) between this Coord and `other`
-    pub fn distance_to(&self, other: &Self) -> f32 {
+    pub fn distance_to(&self, other: &Self) -> f64 {
         let x_diff = self.x - other.x;
         let y_diff = self.y - other.y;
         ((x_diff * x_diff) + (y_diff * y_diff))
-            .to_f32()
+            .to_f64()
             .unwrap()
             .sqrt()
+    }
+
+    /// Calculate the Manhattan distance between this Coord and `other`
+    pub fn manhattan_distance(&self, other: &Self) -> f64 {
+        let x_diff = self.x - other.x;
+        let y_diff = self.y - other.y;
+        num::abs(x_diff.to_f64().unwrap()) + num::abs(y_diff.to_f64().unwrap())
     }
 }
 
@@ -31,15 +38,6 @@ impl<T> Coord<T>
 where
     T: Integer + ToPrimitive + Copy,
 {
-    /// Calculate the Manhattan distance between this Coord and `other`
-    pub fn manhattan_distance(&self, other: &Self) -> u64 {
-        let x_diff = self.x - other.x;
-        let y_diff = self.y - other.y;
-        (num::abs(x_diff.to_i64().unwrap()) + num::abs(y_diff.to_i64().unwrap()))
-            .to_u64()
-            .unwrap()
-    }
-
     /// Returns a slice containing the 8 surrounding neighbors to this Coord
     pub fn neighbors(&self) -> [Self; 8] {
         [
@@ -141,16 +139,29 @@ mod tests {
 
     #[test]
     fn euclidean_distance() {
-        assert_eq!(1.4142135, Coord::new(0, 0).distance_to(&Coord::new(1, 1)));
         assert_eq!(
-            1.4142135,
+            1.4142135623730951,
+            Coord::new(0, 0).distance_to(&Coord::new(1, 1))
+        );
+        assert_eq!(
+            1.4142135623730951,
             Coord::new(0., 0.).distance_to(&Coord::new(-1., -1.))
         );
     }
 
     #[test]
     fn manhattan_distance() {
-        assert_eq!(6, Coord::new(0, 0).manhattan_distance(&Coord::new(3, 3)));
-        assert_eq!(6, Coord::new(0, 0).manhattan_distance(&Coord::new(-3, -3)));
+        assert_eq!(
+            6,
+            Coord::new(0, 0).manhattan_distance(&Coord::new(3, 3)) as u64
+        );
+        assert_eq!(
+            6,
+            Coord::new(0, 0).manhattan_distance(&Coord::new(-3, -3)) as u64
+        );
+        assert_eq!(
+            2.5,
+            Coord::new(0., 0.).manhattan_distance(&Coord::new(0.0, -2.5))
+        );
     }
 }
